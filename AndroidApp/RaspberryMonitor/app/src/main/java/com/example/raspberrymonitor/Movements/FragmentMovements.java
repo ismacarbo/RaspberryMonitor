89b45a2c7f33d260261176dbb446ed60b1072712
Ismaele <Ismaele.carbonari@buonarroti.tn.it>
2024-08-05 00:55:02 +0200

    added real time visualization of esp32 image data

--- AndroidApp/RaspberryMonitor/app/src/main/java/com/example/raspberrymonitor/Movements/FragmentMovements.java
+++ AndroidApp/RaspberryMonitor/app/src/main/java/com/example/raspberrymonitor/Movements/FragmentMovements.java
@@ -1,9 +1,13 @@
 package com.example.raspberrymonitor.Movements;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +22,7 @@ public class FragmentMovements extends Fragment {
     private MainViewModel viewModel;
     private TextView timestampTextView;
     private TextView detailTextView;
+    private WebView webView;
 
     public FragmentMovements() {
         // Required empty public constructor
@@ -30,6 +35,26 @@ public class FragmentMovements extends Fragment {
 
         timestampTextView = view.findViewById(R.id.timestampTextView);
         detailTextView = view.findViewById(R.id.detailTextView);
+        webView = view.findViewById(R.id.webView);
+
+        // Enable JavaScript and other settings for mobile view
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        // Enable mixed content to allow loading HTTP content from HTTPS connection
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        // Set a mobile user agent string
+        webSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.93 Mobile Safari/537.36");
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://192.168.1.116/"); // Replace with the IP address of your ESP32
 
         viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
 
@@ -37,7 +62,6 @@ public class FragmentMovements extends Fragment {
             @Override
             public void onChanged(MovementsResponse movementsResponse) {
                 if (movementsResponse != null && movementsResponse.getMovements() != null && !movementsResponse.getMovements().isEmpty()) {
-                    // Assuming we are interested in the latest movement
                     MovementsResponse.Movement latestMovement = movementsResponse.getMovements().get(movementsResponse.getMovements().size() - 1);
                     timestampTextView.setText("Timestamp: " + latestMovement.getTimestamp());
                     detailTextView.setText("Detail: " + latestMovement.getDetail());
--- ArduinoScript/movementsController/movementsController.ino
+++ ArduinoScript/movementsController/movementsController.ino
@@ -1,88 +1,252 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 
-// Define the pin for PIR sensor
-const int pirPin = D2;   // Pin of the PIR sensor
+// Configurazione WiFi
+const char* ssid = "Telecom casa";
+const char* password = "40166575";
 
-// WiFi configuration
-const char* ssid = "your_ssid";
-const char* password = "your_password";
+// Endpoint del server Flask
+const char* loginServerName = "https://ismacarbo.zapto.org/login";
+const char* movementsServerName = "https://ismacarbo.zapto.org/api/movements";
 
-// Flask endpoints
-const char* loginServerName = "https://yourserver.com/login";
-const char* movementsServerName = "https://yourserver.com/api/movements";
-
-// SSL fingerprint of your server (use your server's SSL fingerprint)
-const char* fingerprint = "your_server_fingerprint";
-
-// Store JWT token
+// Token JWT
 String jwtToken;
 
-// Create a secure WiFiClient
+// Creare un WiFiClient sicuro
 WiFiClientSecure client;
 
-void setup() {
-  pinMode(pirPin, INPUT);    // Set the PIR pin as input
-  Serial.begin(115200);      // Start the serial communication
+// Configurazione della fotocamera
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
 
-  // Connect to WiFi
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+camera_fb_t* previousFrame = NULL; // Memorizza il frame precedente per il rilevamento del movimento
+
+WebServer server(80);
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
   WiFi.begin(ssid, password);
-  Serial.println("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.print(".");
+    Serial.println("Connessione a WiFi...");
   }
-  Serial.println("\nConnected to WiFi");
+  Serial.println("Connesso a WiFi");
+  Serial.println(WiFi.localIP());
 
-  // Set the SSL fingerprint
-  client.setFingerprint(fingerprint);
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-  // Perform login to obtain JWT token
-  if (performLogin("your_username", "your_password")) {
-    Serial.println("Login successful");
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
   } else {
-    Serial.println("Login failed");
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
   }
+
+  // Inizializzazione della fotocamera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Inizializzazione fotocamera fallita con errore 0x%x", err);
+    return;
+  }
+
+  // Effettua il login per ottenere il token JWT
+  if (performLogin("admin", "admin")) {
+    Serial.println("Login riuscito");
+  } else {
+    Serial.println("Login fallito");
+  }
+
+  startCameraServer();
 }
 
 void loop() {
-  int pirState = digitalRead(pirPin);  // Read the state of the PIR sensor
-
-  if (pirState == HIGH) {
-    if (!sendMovementDetected()) {
-      // If sending movement data fails due to expired token, try logging in again
-      if (performLogin("your_username", "your_password")) {
-        Serial.println("Re-login successful");
-        // Try sending the movement data again after re-login
-        sendMovementDetected();
-      } else {
-        Serial.println("Re-login failed");
-      }
-    }
-    Serial.println("Movement detected!");  // Print message to serial
-  } else {
-    Serial.println("No movement.");  // Print message to serial
+  server.handleClient();
+  // Cattura un frame
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Cattura fotocamera fallita");
+    return;
   }
 
-  delay(1000);  // Pause for 10 seconds between requests
+  // Esegue il rilevamento del movimento
+  bool motionDetected = detectMotion(fb);
+  
+  if (motionDetected) {
+    if (!sendMovementDetected()) {
+      // Se l'invio dei dati di movimento fallisce a causa di un token scaduto, prova a effettuare nuovamente il login
+      if (performLogin("admin", "admin")) {
+        Serial.println("Re-login riuscito");
+        // Prova a inviare nuovamente i dati di movimento dopo il re-login
+        sendMovementDetected();
+      } else {
+        Serial.println("Re-login fallito");
+      }
+    }
+    Serial.println("Movimento rilevato!");  // Stampa un messaggio sul seriale
+  }
+
+  // Restituisce il buffer del frame al driver
+  esp_camera_fb_return(fb);
+
+  delay(1000);  // Pausa di 1 secondo tra i controlli
+}
+
+void handle_jpg_stream() {
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t * _jpg_buf = NULL;
+  char * part_buf[64];
+  static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=frame";
+  static const char* _STREAM_BOUNDARY = "\r\n--frame\r\n";
+  static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+  WiFiClient client = server.client();
+  if (!client.connected()) {
+    return;
+  }
+
+  client.printf("HTTP/1.1 200 OK\r\n");
+  client.printf("Content-Type: %s\r\n\r\n", _STREAM_CONTENT_TYPE);
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if (fb->format != PIXFORMAT_JPEG) {
+        bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+        if (!jpeg_converted) {
+          Serial.println("JPEG compression failed");
+          esp_camera_fb_return(fb);
+          res = ESP_FAIL;
+        }
+      } else {
+        _jpg_buf_len = fb->len;
+        _jpg_buf = fb->buf;
+      }
+    }
+    if (res == ESP_OK) {
+      client.printf(_STREAM_BOUNDARY);
+      client.printf(_STREAM_PART, _jpg_buf_len);
+      client.write(_jpg_buf, _jpg_buf_len);
+    }
+    if (fb) {
+      esp_camera_fb_return(fb);
+      _jpg_buf = NULL;
+    } else if (_jpg_buf) {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (!client.connected()) {
+      break;
+    }
+  }
+}
+
+void startCameraServer() {
+  server.on("/", HTTP_GET, handle_jpg_stream);
+  server.begin();
+}
+
+bool detectMotion(camera_fb_t * frame) {
+  if (previousFrame == NULL) {
+    // Se non c'è un frame precedente, memorizza il frame corrente e restituisci che non è stato rilevato alcun movimento
+    previousFrame = frame;
+    return false;
+  }
+
+  // Confronta il frame corrente con il frame precedente
+  bool motionDetected = false;
+  int threshold = 20; // Soglia per il rilevamento del movimento
+  int motionCount = 0;
+  int pixelCount = frame->len;
+
+  // Metodo semplice di differenza di pixel
+  for (int i = 0; i < pixelCount; i++) {
+    if (abs(frame->buf[i] - previousFrame->buf[i]) > threshold) {
+      motionCount++;
+    }
+  }
+
+  // Imposta il rilevamento del movimento se un numero significativo di pixel differisce
+  if (motionCount > pixelCount * 0.1) { // Regola la sensibilità secondo necessità
+    motionDetected = true;
+  }
+
+  // Libera il frame precedente e aggiornalo al frame corrente
+  esp_camera_fb_return(previousFrame);
+  previousFrame = frame;
+
+  return motionDetected;
 }
 
 bool performLogin(const char* username, const char* password) {
   if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Inizio login...");
+    client.setInsecure(); // Ignora la verifica del certificato
     HTTPClient http;
-    http.begin(client, loginServerName);  // Use updated API
+    http.begin(client, loginServerName);  // Usa l'API aggiornata
 
     http.addHeader("Content-Type", "application/json");
     String payload = "{\"username\":\"" + String(username) + "\",\"password\":\"" + String(password) + "\"}";
+    Serial.print("Payload: ");
+    Serial.println(payload);
     int httpResponseCode = http.POST(payload);
+
+    Serial.print("Codice di risposta HTTP: ");
+    Serial.println(httpResponseCode);
 
     if (httpResponseCode == 200) {
       String response = http.getString();
-      Serial.println("Login response:");
+      Serial.println("Risposta login:");
       Serial.println(response);
 
-      // Extract JWT token from the response
+      // Estrai il token JWT dalla risposta
       int tokenIndex = response.indexOf("access_token\":\"") + 15;
       int tokenEndIndex = response.indexOf("\"", tokenIndex);
       jwtToken = response.substring(tokenIndex, tokenEndIndex);
@@ -90,47 +254,55 @@ bool performLogin(const char* username, const char* password) {
       http.end();
       return true;
     } else {
-      Serial.print("Error on sending POST: ");
+      Serial.print("Errore durante l'invio del POST: ");
       Serial.println(httpResponseCode);
+      String response = http.getString();
+      Serial.println("Risposta dal server:");
+      Serial.println(response);
       http.end();
       return false;
     }
   } else {
-    Serial.println("Error in WiFi connection");
+    Serial.println("Errore nella connessione WiFi");
     return false;
   }
 }
 
 bool sendMovementDetected() {
   if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Invio dati movimento...");
+    client.setInsecure(); // Ignora la verifica del certificato
     HTTPClient http;
-    http.begin(client, movementsServerName);  // Use updated API
+    http.begin(client, movementsServerName);  // Usa l'API aggiornata
 
     http.addHeader("Content-Type", "application/json");
     http.addHeader("Authorization", "Bearer " + jwtToken);
     String payload = "{\"movement\": \"detected\"}";
     int httpResponseCode = http.POST(payload);
 
+    Serial.print("Codice di risposta HTTP: ");
+    Serial.println(httpResponseCode);
+
     if (httpResponseCode == 200) {
       String response = http.getString();
-      Serial.println(httpResponseCode);   // Print response code
-      Serial.println(response);           // Print server response
-      http.end();  // Close connection
+      Serial.println(httpResponseCode);   // Stampa il codice di risposta
+      Serial.println(response);           // Stampa la risposta del server
+      http.end();  // Chiude la connessione
       return true;
     } else if (httpResponseCode == 401) {
       String response = http.getString();
-      Serial.println(httpResponseCode);   // Print response code
-      Serial.println(response);           // Print server response
+      Serial.println(httpResponseCode);   // Stampa il codice di risposta
+      Serial.println(response);           // Stampa la risposta del server
       if (response.indexOf("Token has expired") >= 0) {
-        Serial.println("Token has expired, need to re-login");
+        Serial.println("Token scaduto, è necessario effettuare nuovamente il login");
       }
     } else {
-      Serial.print("Error on sending POST: ");
+      Serial.print("Errore durante l'invio del POST: ");
       Serial.println(httpResponseCode);
     }
-    http.end();  // Close connection
+    http.end();  // Chiude la connessione
   } else {
-    Serial.println("Error in WiFi connection");
+    Serial.println("Errore nella connessione WiFi");
   }
   return false;
 }
